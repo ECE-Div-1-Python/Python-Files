08991c74d2bcbd93a78ab9e348520d3bb2b74813
astrov11 <vidhi011@gmail.com>
2025-04-25 00:18:42 +0530

    Add files via upload

---
+++ 24BEC010/roll no, name, age.py
@@ -0,0 +1,10 @@
+l=[(1, 'ruchita', 18), (2, 'name2', 18), (3, 'name3', 19)]
+print(l)
+l1=[]
+l2=[]
+l3=[]
+for i in l:
+    l1.append(i[0])
+    l2.append(i[1])
+    l3.append(i[2])
+print(l1,l2,l3,sep='\n')
